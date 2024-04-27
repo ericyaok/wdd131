79e6bcbca5495acc05ac1ed2d5ac48d95d349ab5
Eric Kodzi <erickodzi@gmail.com>
2024-04-27 13:53:32 +0100

    Homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,10 @@
+const today = new Date();
+const year = today.getFullYear();
+
+let yearSpan = document.getElementById("currentyear");
+yearSpan.innerHTML = year;
+
+const lastModified = document.lastModified;
+
+let modifiedspan = document.getElementById("modified");
+modifiedspan.innerHTML = lastModified;
